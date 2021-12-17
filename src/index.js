efcc07637b784e03b2de911a1c496dd24fee8028
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2021-12-17 14:16:52 -0500

    :zap: Added Breakpoints and a custom theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-ReactDOM.render( <App/>,document.getElementById('root'));
+
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+});
+
+const customTheme = {
+  fonts: {
+    body: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif",
+  },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "#F5f5f5",
+      },
+    },
+  },
+};
+
+const theme = extendTheme({ ...customTheme, breakpoints });
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>,
+  document.getElementById("root")
+);
